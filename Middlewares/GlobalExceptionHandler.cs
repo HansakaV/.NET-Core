b9509397ec905ee48c3a720ed25cd92daf68d225
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-12 22:25:30 +0530

    feat:develop global exception Handling Middleware

---
+++ Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.API.Middlewares
+{
+    public class GlobalExceptionHandler
+    {
+        
+    }
+}
