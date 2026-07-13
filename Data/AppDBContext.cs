6f12799ed9f2a3b18dc5523a6cd2e6a51f350dc6
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-13 20:28:36 +0530

    refactor:updated dbContext

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -7,5 +7,12 @@ public class AppDBContext : DbContext
     public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+            .Property(s => s.Course)
+            .HasConversion<string>();
+    }
     public DbSet<Student> Students { get; set; }    
+    public DbSet<User> Users {get; set;}
 }
