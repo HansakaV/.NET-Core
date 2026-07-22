b2a230ce00b7fcc9d04ebc0193ae2e25e0faf936
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-22 14:08:28 +0530

    feat:relationship configurd

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -13,18 +13,23 @@ public class AppDBContext : DbContext
             .Property(s => s.Course)
             .HasConversion<string>();
         
+        modelBuilder.Entity<Student>()
+            .HasOne(s => s.Course)
+            .WithMany(c => c.Students)
+            .HasForeignKey(s => s.CourseId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        
         modelBuilder.Entity<Student>(property =>
         {
-            property.HasIndex(s => s.Email)
-                .IsUnique();
+            property.HasIndex(s => s.Email).IsUnique();
             property.HasIndex(s => s.Name);
-            property.HasIndex(s => s.Course);
+            property.HasIndex(s => s.CourseId);
 
         });
         modelBuilder.Entity<User>(property =>
         {
-            property.HasIndex(u => u.Email)
-                .IsUnique();
+            property.HasIndex(u => u.Email).IsUnique();
         });
     }
     public DbSet<Student> Students { get; set; }    
