0ea2f0c55ae0ed2bfda24ba94e9a50f7149ca3aa
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-22 13:54:32 +0530

    refactor:intilalized one to many relationship

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,11 @@
-using StudentManagement.API.Enums;
-
 namespace StudentManagement.API.Models;
     public class Student
 {
     public int Id{get; set;}
     public string Name{get; set;} = string.Empty;
     public string Email{get; set;} = string.Empty;
-    public Courses Course{get; set;} 
     public string Phone{get; set;} = string.Empty;
+    public int CourseId {get;set;}
+    public Course Course{get;set;} =null!;
 
 }
