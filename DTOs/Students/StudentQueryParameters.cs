4831519f79770ff67d851f6bc09b77511e484fa6
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-22 13:30:43 +0530

    refactor:updated student query params

--- DTOs/Students/StudentQueryParameters.cs
+++ DTOs/Students/StudentQueryParameters.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using StudentManagement.API.Enums;
 
 namespace StudentManagement.API.DTOs.Students
 {
@@ -8,5 +9,9 @@ namespace StudentManagement.API.DTOs.Students
          public int page {get;set;} = 1;
         [Range(1,100)]
         public int pageSize {get;set;} = 10;
+        public string? SearchTerm{get;set;}
+        public string? Sortby{get;set;} = "id";
+        public bool IsDecending{get;set;} = false;
+        public Courses? Course{get;set;}
     }
 }
