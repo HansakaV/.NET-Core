3be0ef4962bfee75e423e7e45f7cb3114a5368a9
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-22 16:44:55 +0530

    refactor:add new CourseId property

--- DTOs/Students/StudentResponseDto.cs
+++ DTOs/Students/StudentResponseDto.cs
@@ -7,6 +7,7 @@ namespace StudentManagement.API.DTOs
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Course {get;set;} = string.Empty;
+        public int CourseId {get;set;}
         public string Phone { get; set; } = string.Empty;
     }
 }
