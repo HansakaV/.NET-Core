71c4f0e7d3db1ec7f95d82a27020b0a17a49ea57
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-26 00:15:29 +0530

    add new ip address as a parameter on login

--- Interfaces/IAuthService.cs
+++ Interfaces/IAuthService.cs
@@ -4,7 +4,7 @@ namespace StudentManagement.API.Interfaces
 {
     public interface IAuthService
     {
-        Task<LoginResponseDto> LoginAsync(LoginRequestDto loginRequest);
+        Task<AuthenticationResultDto> LoginAsync(LoginRequestDto loginRequest, string? ipaddres);
         Task<string> RegisterAsync(RegisterRequestDto registerRequest);
         Task<bool> ForgotPasswordAsync(ForgotPasswordRequestDto forgotPasswordRequest);
         Task<bool> SendVerifactionCodeAsync(SendVerificationCodeDto sendVerificationCode);
