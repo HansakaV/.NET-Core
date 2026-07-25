c6302021406f01b435512d863f3f83c4dd7f3f02
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-26 00:18:04 +0530

    feat:add new refreshAsync

--- Interfaces/IAuthService.cs
+++ Interfaces/IAuthService.cs
@@ -8,5 +8,6 @@ namespace StudentManagement.API.Interfaces
         Task<string> RegisterAsync(RegisterRequestDto registerRequest);
         Task<bool> ForgotPasswordAsync(ForgotPasswordRequestDto forgotPasswordRequest);
         Task<bool> SendVerifactionCodeAsync(SendVerificationCodeDto sendVerificationCode);
+        Task<AuthenticationResultDto> RefreshAsync(string rawRefreshToken , string? ipaddres);
     }
 }
