ec3d2becec4cb7247dcc641c18d38fed93ac96f3
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-25 19:24:58 +0530

    feat:implemented refresh token entitiy

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,24 @@
+namespace StudentManagement.API.Models
+{
+    public class RefreshToken
+    {
+        public int Id {get;set;}
+        public string TokenHash {get;set;} = string.Empty;
+        public DateTime ExpiresAt {get;set;}
+        public DateTime? RevokedAt {get;set;}
+        //Hash New Token In Rotation
+        public string? ReplacedByTokenHash {get;set;}
+        public string? CreatedByIp {get;set;}
+        public string? RevoekdByIp {get;set;}
+        //foreign Refernce
+        public int UserId {get;set;}
+        public User user {get;set;} = null!;
+        //computed Properties
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        public bool ISRevoked => RevokedAt is not null;
+        public bool IsActive => !IsExpired && !ISRevoked;
+        
+    
+
+    }
+}
