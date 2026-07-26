d49e56e328b584e1a97585dde65ee65d577796ff
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-26 19:17:26 +0530

    feat:added version property

--- Models/Student.cs
+++ Models/Student.cs
@@ -9,5 +9,6 @@ namespace StudentManagement.API.Models;
     public string Phone{get; set;} = string.Empty;
     public int CourseId {get;set;}
     public Course Course{get;set;} =null!;
+    public int Version {get;set;} = 1;
 
 }
