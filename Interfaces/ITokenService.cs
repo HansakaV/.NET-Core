405821ac3915787109ec56c7f3a74b10fb991074
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-26 13:52:08 +0530

    feat:developed token service interface

---
+++ Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+using StudentManagement.API.Models;
+
+namespace StudentManagement.API.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(User user);
+        string GenerateRefreshToken();
+        string HashToken(string token);
+    }
+}
