bdbc8a31e0a9934d858ce98073e68ab5366e2352
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-30 22:07:43 +0530

    feat:developed enrollment entity

---
+++ Models/Enrollment.cs
@@ -0,0 +1,13 @@
+namespace StudentManagement.API.Models
+{
+    public class Enrollment
+    {
+        public int Id {get;set;}
+        public int StuedntId {get;set;}
+        public Student Student {get;set;} = null!;
+        public int CourseId {get;set;}
+        public Course Course {get;set;} = null!;
+        public DateTime EnrolledAt {get;set;} = DateTime.UtcNow;
+
+    }
+}
