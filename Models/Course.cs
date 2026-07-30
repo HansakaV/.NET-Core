2085ec5f92980fb6916aea7eb72a7530032b6d72
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-30 22:03:20 +0530

    Feat:added new availableseats property

--- Models/Course.cs
+++ Models/Course.cs
@@ -6,6 +6,8 @@ namespace StudentManagement.API.Models
         public string CourseCode {get;set;} = string.Empty;
         public string CourseName {get;set;} = string.Empty;
         public int DurationInMonths {get;set;} 
+        public int AvailableSeats {get;set;}
+        public int Version {get;set;} = 1;
         public ICollection<Student> Students {get;set;} = new List<Student>();
     }
 }
