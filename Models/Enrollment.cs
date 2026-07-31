6dfafbdef24db682525de9ad119fdad6d3430f95
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-07-31 09:38:44 +0530

    fix:fixed propert typo

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -3,7 +3,7 @@ namespace StudentManagement.API.Models
     public class Enrollment :BaseEntity
     {
         public int Id {get;set;}
-        public int StuedntId {get;set;}
+        public int StudentId {get;set;}
         public Student Student {get;set;} = null!;
         public int CourseId {get;set;}
         public Course Course {get;set;} = null!;
