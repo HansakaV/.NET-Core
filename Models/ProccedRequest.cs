69f86d001702d98b61e1908ad441fc7ca4b1d9c6
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-08-01 10:31:16 +0530

    reafctor:added Primary Key

--- Models/ProccedRequest.cs
+++ Models/ProccedRequest.cs
@@ -2,6 +2,7 @@ namespace StudentManagement.API.Models
 {
     public class ProccedRequest : BaseEntity
     {
+        public int Id {get;set;}
         public string IdempotencyKey {get;set;} = string.Empty;
         public string ResponsePayload {get;set;} = string.Empty;
         public DateTime ProcessedAt {get;set;} = DateTime.UtcNow;
