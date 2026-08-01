f3d8935d0323cd0ff86910277b55c74ca9077e98
Mahesh Hansaka <maheshhansaka628@gmail.com>
2026-08-01 10:34:46 +0530

    refactor:added new propety idempotencykey

--- DTOs/Enrollment/EnrollmentRequestDTO.cs
+++ DTOs/Enrollment/EnrollmentRequestDTO.cs
@@ -2,6 +2,7 @@ namespace StudentManagement.API.DTOs.Enrollment
 {
     public class EnrollmentRequestDTO
     {
+        public string IdempotencyKey {get;set;} = string.Empty;
         public int StudentId {get;set;}
         public int CourseId {get;set;} 
     }
